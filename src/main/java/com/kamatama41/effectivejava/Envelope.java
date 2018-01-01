8c2b2452ced9d492bfa3a0646fa49f5d6f96eba3
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-01 18:13:42 +0900

    Add some tests

--- src/main/java/com/kamatama41/effectivejava/Envelope.java
+++ src/main/java/com/kamatama41/effectivejava/Envelope.java
@@ -1,5 +1,7 @@
 package com.kamatama41.effectivejava;
 
+import java.util.function.Supplier;
+
 /**
  * Item 24: Favor static member class over nonstatic
  */
@@ -13,6 +15,30 @@ public class Envelope {
         void hello () {
             // This "Envelope.this" is called "qualified this"
             System.out.println("Hello, " + Envelope.this.getName());
+
+            // Local class
+            class HelloEnclosure {
+                private String getClassName() {
+                    return this.getClass().toString();
+                }
+            }
+            // class com.kamatama41.effectivejava.Envelope$Enclosure$1HelloEnclosure
+            System.out.println(new HelloEnclosure().getClassName());
+
+            // Anonymous class
+            Supplier<String> s = new Supplier<String>() {
+                @Override
+                public String get() {
+                    return this.getClass().toString();
+                }
+            };
+            // class com.kamatama41.effectivejava.Envelope$Enclosure$1
+            System.out.println(s.get());
+
+            // Lambda
+            s = () -> this.getClass().toString();
+            // class com.kamatama41.effectivejava.Envelope$Enclosure
+            System.out.println(s.get());
         }
     }
 
