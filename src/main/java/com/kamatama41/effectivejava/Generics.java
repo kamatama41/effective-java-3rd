99d4e8c6daf0482e1aba0c6a7d84a36b5da198e6
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-02 17:04:27 +0900

    Add test for bounded wildcards

--- src/main/java/com/kamatama41/effectivejava/Generics.java
+++ src/main/java/com/kamatama41/effectivejava/Generics.java
@@ -1,7 +1,10 @@
 package com.kamatama41.effectivejava;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Generics {
@@ -13,6 +16,13 @@ public class Generics {
         List<?> l2 = test2(1, "a");
         // l2.add(3f); Error
         System.out.println(l2);
+
+        /**
+         * Test for bounded wildcards
+         */
+        List<MyNumber> numbers = Arrays.asList(new MyNumber("1"), new MyNumber("1.5"), new MyNumber("2"));
+        MyNumber max = max(numbers);
+        System.out.println(max);
     }
 
     static List test1(Object... objects) {
@@ -21,5 +31,21 @@ public class Generics {
 
     static List<?> test2(Object... objects) {
         return Arrays.stream(objects).collect(Collectors.toList());
+    }
+
+    static <E extends Comparable<? super E>> E max(Collection<? extends E> c) {
+        E result = null;
+        for (E e : c) {
+            if (result == null || e.compareTo(result) > 0) {
+                result = Objects.requireNonNull(e);
+            }
+        }
+        return result;
+    }
+
+    static class MyNumber extends BigDecimal {
+        public MyNumber(String val) {
+            super(val);
+        }
     }
 }
