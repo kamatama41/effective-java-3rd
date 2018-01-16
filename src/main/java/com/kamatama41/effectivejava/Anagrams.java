d615803ec5c916df2060a004610ab0ad72359117
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-16 22:02:33 +0900

    2nd version of Anagrams

--- src/main/java/com/kamatama41/effectivejava/Anagrams.java
+++ src/main/java/com/kamatama41/effectivejava/Anagrams.java
@@ -1,31 +1,29 @@
 package com.kamatama41.effectivejava;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.groupingBy;
+
+// Overuse of streams - don't do this!
 public class Anagrams {
     public static void main(String[] args) throws IOException {
-        File dictionary = new File(args[0]);
+        Path dictionary = Paths.get(args[0]);
         int minGroupSize = Integer.parseInt(args[1]);
-        Map<String, Set<String>> groups = new HashMap<>();
 
-        try(Scanner s = new Scanner(dictionary)) {
-            while(s.hasNext()) {
-                String word = s.next();
-                groups.computeIfAbsent(alphabetize(word),
-                        (unused) -> new TreeSet<>()).add(word);
-            }
+        try(Stream<String> words = Files.lines(dictionary)) {
+            words.collect(
+                    groupingBy(word -> word.chars()
+                            .sorted()
+                            .collect(StringBuilder::new, (sb, c) -> sb.append((char) c), StringBuilder::append)
+                            .toString()
+            )).values().stream()
+            .filter(group -> group.size() >= minGroupSize)
+            .map(group -> group.size() + ": " + group)
+            .forEach(System.out::println);
         }
-        for (Set<String> group : groups.values()) {
-            if (group.size() >= minGroupSize)
-                System.out.println(group.size() + ": " + group);
-        }
-    }
-
-    private static String alphabetize(String s) {
-        char[] a = s.toCharArray();
-        Arrays.sort(a);
-        return new String(a);
     }
 }
